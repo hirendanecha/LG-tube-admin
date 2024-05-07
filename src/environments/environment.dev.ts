f5533abcb3b6310ce747d8b40e415b7a61fb0b85
vatsal <vso.opash@gmail.com>
2024-05-07 15:31:48 +0530

    change api

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: false,
   hmr: false,
-  serverUrl: 'https://dev-api.latinagirls.tube/api/v1/',
+  serverUrl: 'https://api.latinagirls.tube/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://dev-api.latinagirls.tube/',
+  socketUrl: 'https://api.latinagirls.tube/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
+
 };
